582fb83d975ed741ad182bdb4d1ec557184515d4
srijal <srijal@srijal-ThinkPad-T14-Gen-2i>
2025-01-13 16:27:29 +0545

    adding env for MONGO_URL and docker-compose.yaml

--- api/index.js
+++ api/index.js
@@ -9,7 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/3tier', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// Connect to MongoDB using an environment variable
+if (!process.env.MONGO_URI) {
+  throw new Error('MONGO_URI environment variable is not set');
+}
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log('Failed to connect to MongoDB:', err));
 
